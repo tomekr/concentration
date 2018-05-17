9742e073ef52cda77b24ee901e7b87fefa5a4036
Tomek Rabczak <tomek.rabczak@getbraintree.com>
2018-05-17 00:13:42 -0500

    Finished up lecture 2

--- Concentration/Concentration.swift
+++ Concentration/Concentration.swift
@@ -12,11 +12,27 @@ class Concentration
 {
     var cards = [Card]()
     
+    var indexOfOneAndOnlyFaceCard: Int?
+    
     func chooseCard(at index: Int) {
-        if cards[index].isFaceUp {
-            cards[index].isFaceUp = false
-        } else {
-            cards[index].isFaceUp = true
+        // This is where the meat of our game logic lives
+        if !cards[index].isMatched {
+            if let matchIndex = indexOfOneAndOnlyFaceCard, matchIndex != index {
+                if cards[matchIndex].identifier == cards[index].identifier {
+                    cards[matchIndex].isMatched = true
+                    cards[index].isMatched = true
+                }
+                cards[index].isFaceUp = true
+                indexOfOneAndOnlyFaceCard = nil
+            } else {
+                // zero or two cards are selected
+                for flipDownIndex in cards.indices {
+                    cards[flipDownIndex].isFaceUp = false
+                }
+                cards[index].isFaceUp = true
+                indexOfOneAndOnlyFaceCard = index
+            }
+            
         }
     }
     
