6069668e4ee26e49c8592ed36af01d353e550287
Tomek Rabczak <tomek.rabczak@getbraintree.com>
2018-05-16 23:50:28 -0500

    sort of working implementation

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -9,7 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-    var game: Concentration = Concentration()
+    // If you make a var lazy, it doesn't initialize until someone tries to use it.
+    // The downside is that you can't use didSet. Also no one can use game until
+    // cardButtons is initialized
+    lazy var game = Concentration(numberOfPairsOfCards: (cardButtons.count + 1) / 2)
     
     // Instance variable (i.e. property). All instance variables
     // need to be initialized.
@@ -21,27 +24,51 @@ class ViewController: UIViewController {
     }
     
     @IBOutlet var cardButtons: [UIButton]!
-    var emojiChoices = ["🎃", "👻", "🤡", "👅"]
     
     @IBOutlet weak var flipCountLabel: UILabel!
     
     @IBAction func touchCard(_ sender: UIButton) {
         flipCount += 1
-        // use let instead of var because this is a constant
-        let cardNumber = cardButtons.index(of: sender)!
-        print("cardNumber = \(cardNumber)")
-        flipCard(withEmoji: emojiChoices[cardNumber], on: sender)
+        if let cardNumber = cardButtons.index(of: sender) {
+            game.chooseCard(at: cardNumber)
+            updateViewFromModel()
+        } else {
+            print("chosen card was not in cardButtons")
+        }
     }
     
-    func flipCard(withEmoji emoji: String, on button: UIButton) {
-        print("DEBUG: flipCard(withEmoji: \(emoji))")
-        if button.currentTitle == emoji {
-            button.setTitle("", for: UIControlState.normal)
-            button.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
-        } else {
-            button.setTitle(emoji, for: UIControlState.normal)
-            button.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+    func updateViewFromModel() {
+        for index in cardButtons.indices {
+            let button = cardButtons[index]
+            let card = game.cards[index]
+            if card.isFaceUp {
+                button.setTitle(emoji(for: card), for: UIControlState.normal)
+                button.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+            } else {
+                button.setTitle("", for: UIControlState.normal)
+                button.backgroundColor = card.isMatched ? #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0) : #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+            }
         }
+    }
+    
+    var emojiChoices = ["🎃", "👻", "🤡", "👅", "🐃", "🍱", "🍩", "🎬", "🚌"]
+    
+    // same as Dictionary<Int,String>()
+    var emoji = [Int:String]()
+    
+    func emoji(for card: Card) -> String {
+        // you can simulate and && with a comma in Swift
+        if emoji[card.identifier] == nil, emojiChoices.count > 0 {
+            let randomIndex = Int(arc4random_uniform(UInt32(emojiChoices.count)))
+            emoji[card.identifier] = emojiChoices.remove(at: randomIndex)
+        }
+        // The below one-liner is exactly the same as:
+        // if emoji[card.identifier] != nil {
+        //     return emoji[card.identifier]!
+        // } else {
+        //     return "?"
+        // }
+        return emoji[card.identifier] ?? "?"
     }
 }
 
